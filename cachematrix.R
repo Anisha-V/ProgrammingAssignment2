ed8f8dcd51cd406f0c3b4d3da69f5017c6c01c06
Anisha-V <anishaviyer@gmail.com>
2015-06-21 14:49:58 -0400

    Revert "Initial commit for Programming Assignment 2"
    This reverts commit 27987823fcf81d46a5e2186391addd87c079b879.
    
    Conflicts:
    	README.md
    	cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## These functions are used to calculate the inverse of a user defined square matrix. the first function sets the matrix  
+## and calculates its inverse and the second function checks the cache to see if the inverse is already available in the
+## cache.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(solve) m <<- solve
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+##this function first checks if the value of getmatrix function which is the inverse of the matrix is not null,
+## in which case it returns the value from the previous computation. This is only possible if we do not reassign the 
+## set function.  ELSE it gets the newvalues of the matrix and computes the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmatrix(m)
+  m
 }
+## Testing with a square matrix
+test1 <- makeCacheMatrix ()
+test1$set (matrix (c(7,4,3,73,67,45,13,31,12),3,3, byrow = T))
+cacheSolve(test1)
